6e9a980299a28e7eca3e09650fd38fed24dde110
ganeodolu <ganeodolu@gmail.com>
2022-10-08 21:42:50 +0900

    fix: 경로 다시 복구

--- custom-react-router/vite.config.ts
+++ custom-react-router/vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wanted-pre-onboarding-challenge-fe-3/custom-react-router/',  // yarn dev등 로컬에서는 주석처리
   plugins: [react()]
 })
